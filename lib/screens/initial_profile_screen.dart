56a8fcd9223af59797423682d23fc9dfac66edde
Kauanny Vieira <kauanny.vieira@am3solucoes.com.br>
2025-04-23 19:55:11 -0300

    feat: inital screen after create account

---
+++ lib/screens/initial_profile_screen.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:loomi_streaming/screens/home_screen.dart';
+
+import '../components/inputs/custom_text_field_decoration.dart';
+import '../core/services/index.dart';
+import '../modules/methods/show_image_picker.dart';
+
+class InitialProfileScreen extends StatefulWidget {
+  const InitialProfileScreen({super.key});
+
+  @override
+  State<InitialProfileScreen> createState() => _InitialProfileScreenState();
+}
+
+class _InitialProfileScreenState extends State<InitialProfileScreen> {
+  final TextEditingController nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF0D0D0D),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/splash.png', height: 40),
+            const SizedBox(height: 24),
+            const Text("Tell us more!",
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white)),
+            const SizedBox(height: 4),
+            const Text("Complete your profile",
+                style: TextStyle(color: Colors.grey)),
+
+            const SizedBox(height: 32),
+
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () => showImagePickerOptions(context),
+                  child: Container(
+                    width: 116,
+                    height: 116,
+                    decoration: BoxDecoration(
+                      color: const Color(0x333A1C5C),
+                      borderRadius: BorderRadius.circular(28),
+                    ),
+                    child: const Center(
+                      child: Icon(Icons.camera_alt_outlined,
+                          color: Color(0xFFBB86FC), size: 24),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                // Texto ao lado
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'CHOOSE IMAGE',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 14,
+                        letterSpacing: 0.5,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      'A square .jpg, .gif,\nor .png image\n200x200 or larger',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 13,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 32),
+            // Name
+            TextFormField(
+              controller: nameController,
+              style: const TextStyle(color: Colors.white),
+              decoration: CustomTextFieldDecoration.build('Your name'),
+            ),
+
+            const SizedBox(height: 26),
+
+            // Botão Login
+            SizedBox(
+              width: 208,
+              child: OutlinedButton(
+                onPressed: () async {
+                  final response = await editUser(nameController.text);
+                  if(response == "Ok"){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+                  } else{
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(response)),
+                    );
+                  }
+                },
+                style: OutlinedButton.styleFrom(
+                  side: const BorderSide(color: Color(0xFFAA73F0)),
+                  backgroundColor: const Color(0x33BB86FC),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                ),
+                child: const Text("Continue",
+                    style: TextStyle(fontSize: 16, color: Color((0xFFBB86FC)))),
+              ),
+            ),
+
+            Align(
+              child: TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                }, // implementar
+                child: const Text("Back",
+                    style: TextStyle(color: Color(0xFFBB86FC))),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
