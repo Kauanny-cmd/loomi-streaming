93815ed67e529778812d568187f97d0a6842c4ce
Kauanny Vieira <kauanny.vieira@am3solucoes.com.br>
2025-04-25 10:34:30 -0300

    feat: add more services with api

---
+++ lib/modules/methods/index.dart
@@ -0,0 +1,3 @@
+export 'show_delete_account_modal.dart' show showDeleteAccountModal;
+export 'show_delete_plan_modal.dart' show showDeletePlanModal;
+export 'show_image_picker.dart' show showImagePickerOptions;
